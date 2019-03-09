a608148bea76a36f08e28343ea5db47c072232c9
Matias Korhonen <matias@kiskolabs.com>
2019-03-09 14:14:41 +0200

    Fetch data from the Netartmo API

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,66 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+import SecurityContext from "./SecurityContext.js";
 
 class Dashboard extends Component {
+  static contextType = SecurityContext;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: false,
+      loading: false,
+      data: null,
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  async fetchData() {
+    await this.setState({ loading: true, error: null });
+
+    const response = await axios.get("https://api.netatmo.com/api/gethomecoachsdata", {
+      params: { access_token: this.context.accessToken },
+    });
+
+    const { data } = response;
+
+    if (data.error) {
+      this.setState({
+        error: data.error,
+        loading: false,
+      });
+    } else {
+      this.setState({
+        error: null,
+        loading: false,
+        data: data.body,
+      });
+    }
+  }
+
+  async componentDidMount() {
+    await this.fetchData();
+  }
+
   render() {
-    return <div>Dashboard</div>;
+    const { error, loading, data } = this.state;
+
+    if (data) {
+      return (
+        <div>
+          <pre>{JSON.stringify(data, undefined, 2)}</pre>
+        </div>
+      );
+    } else if (loading) {
+      return <div>Loading</div>;
+    } else if (error) {
+      return <div>Error! {error}</div>;
+    } else {
+      return <div>Dashboard</div>;
+    }
   }
 }
 
