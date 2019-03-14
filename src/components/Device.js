b69f0f824cf453bf6d7f479bee4478556364ff3a
Matias Korhonen <matias@kiskolabs.com>
2019-03-14 21:19:03 +0200

    Tweaks the icons

--- src/components/Device.js
+++ src/components/Device.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faExclamationTriangle,
   faHeart,
+  faHeartbeat,
   faLeaf,
-  faStar,
   faThermometerQuarter,
   faTint,
   faVolumeUp,
 } from "@fortawesome/free-solid-svg-icons";
-import { faClock, faStar as faEmptyStar } from "@fortawesome/free-regular-svg-icons";
+import { faClock, faHeart as faEmptyHeart } from "@fortawesome/free-regular-svg-icons";
 import { distanceInWords } from "date-fns";
 
 import NetatmoClient from "../lib/NetatmoClient.js";
@@ -29,11 +30,11 @@ class Device extends Component {
     let stars = [];
 
     for (let i = 1; i <= fullStars; i++) {
-      stars.push(<FontAwesomeIcon key={`star-${i}`} icon={faStar} />);
+      stars.push(<FontAwesomeIcon fixedWidth key={`star-${i}`} icon={faHeart} />);
     }
 
     for (let i = 1; i <= emptyStars; i++) {
-      stars.push(<FontAwesomeIcon key={`empty-${i}`} icon={faEmptyStar} />);
+      stars.push(<FontAwesomeIcon fixedWidth key={`empty-${i}`} icon={faEmptyHeart} />);
     }
 
     return stars;
@@ -49,35 +50,35 @@ class Device extends Component {
         <h1>{station_name}</h1>
         <dl>
           <dt>
-            <FontAwesomeIcon icon={faThermometerQuarter} /> Temperature
+            <FontAwesomeIcon fixedWidth size="lg" icon={faThermometerQuarter} /> Temperature
           </dt>
           <dd className={this.netatmoClient.temperatureToColor(dashboard_data.Temperature)}>
             {dashboard_data.Temperature}°C
           </dd>
 
           <dt>
-            <FontAwesomeIcon icon={faTint} /> Humidity
+            <FontAwesomeIcon fixedWidth size="lg" icon={faTint} /> Humidity
           </dt>
           <dd className={this.netatmoClient.humidityToColor(dashboard_data.Humidity)}>
             {dashboard_data.Humidity}%
           </dd>
 
           <dt>
-            <FontAwesomeIcon icon={faLeaf} /> CO₂
+            <FontAwesomeIcon fixedWidth size="lg" icon={faLeaf} /> CO₂
           </dt>
           <dd className={this.netatmoClient.co2ToColor(dashboard_data.CO2)}>
             {dashboard_data.CO2} ppm
           </dd>
 
           <dt>
-            <FontAwesomeIcon icon={faVolumeUp} /> Noise
+            <FontAwesomeIcon fixedWidth size="lg" icon={faVolumeUp} /> Noise
           </dt>
           <dd className={this.netatmoClient.noiseToColor(dashboard_data.Noise)}>
             {dashboard_data.Noise} dB
           </dd>
 
           <dt>
-            <FontAwesomeIcon icon={faHeart} /> Health Index
+            <FontAwesomeIcon fixedWidth size="lg" icon={faHeartbeat} /> Health Index
           </dt>
           <dd className={this.netatmoClient.healthIndexToColor(dashboard_data.health_idx)}>
             {this.renderHealthIndex(dashboard_data.health_idx)}{" "}
@@ -85,7 +86,7 @@ class Device extends Component {
           </dd>
 
           <dt>
-            <FontAwesomeIcon icon={faClock} /> Last update
+            <FontAwesomeIcon fixedWidth size="lg" icon={faClock} /> Last update
           </dt>
           <dd>{distanceInWords(new Date(), last_status_store, { addSuffix: true })}</dd>
         </dl>
